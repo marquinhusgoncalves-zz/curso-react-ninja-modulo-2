2a8e10b5b5ac27dc4cbdb48d5eeb294517a1fd7c
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-12-11 22:13:13 -0200

    M2#A06

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,17 @@
 'use strict'
 
-it('Um é igaul a 1', () => {
-  // console.assert(1 === 1, '1 não é igual a 2')
-  expect(1).toBe(1)
+const expect = require('chai').expect
+const sum = require('./sum')
+
+it('sum should be a function', () => {
+  expect(sum).to.be.a('function')
 })
+
+// it('Um é igaul a 1', () => {
+  // console.assert(1 === 1, '1 não é igual a 2')
+  // Usando funções doJest
+  // expect(1).toBe(1)
+// })
 
 // Exemple
 // describe('# ESCOPO', () => {
