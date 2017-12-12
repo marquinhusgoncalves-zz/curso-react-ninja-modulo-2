dedd58668c265615690b081f4b0a800ac77c4806
Marcus Goncalves <marcus.goncalves1@walmart.com>
2017-12-11 22:01:02 -0200

    M2#A05

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,17 @@
 'use strict'
 
-it('Testando Jest', () => {
-
+it('Um é igaul a 1', () => {
+  // console.assert(1 === 1, '1 não é igual a 2')
+  expect(1).toBe(1)
 })
+
+// Exemple
+// describe('# ESCOPO', () => {
+//   it('Testando Jest', () => {})
+//   it('Testando Jest 2', () => { })
+// })
+
+// describe('# ESCOPO 2', () => {
+//   it('Testando Jest', () => { })
+//   it('Testando Jest 2', () => { })
+// })
