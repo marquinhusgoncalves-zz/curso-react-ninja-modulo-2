89d949caed4f958c2da205c21b60e7e451a9a326
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-06-26 22:17:49 -0300

    M2#A10

--- map/map.js
+++ map/map.js
@@ -1,5 +1,14 @@
-const map = () => {
-  return [1, 2];
+const map = (arr = [], func = (item) => item) => {
+  if (typeof func !== 'function') {
+    throw new TypeError('func is not a function');
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(func(arr[i], i, arr));
+  }
+  return newArr;
 };
 
 export default map;
