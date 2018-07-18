82ab2e80f41a0083465dd39f3d79471780060a3d
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-07-17 22:47:01 -0300

    M2#A12

---
+++ map-recursive/map-recursive.js
@@ -0,0 +1,10 @@
+const map = (arr = [], func = (item) => item) => {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const [head, ...tail] = arr;
+  return [func(head)].concat(map(tail, func));
+};
+
+export default map;
