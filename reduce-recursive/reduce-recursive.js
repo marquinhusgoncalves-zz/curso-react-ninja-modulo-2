27b37b8280b7f9270350f56ecefbe687c314a5d9
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-08-16 21:38:18 -0300

    M2#A24

---
+++ reduce-recursive/reduce-recursive.js
@@ -0,0 +1,16 @@
+const isInitialValueUndefined = initialValue => initialValue === undefined;
+
+const reduce = (arr, func, initialValue) => {
+  let acc = isInitialValueUndefined(initialValue) ? arr[0] : initialValue;
+  let arrCopy = isInitialValueUndefined(initialValue) ? arr.slice(1) : arr;
+
+  return (function reduceInternal(accInternal, arrInternal, counter) {
+    const [head, ...tail] = arrInternal;
+    const accNext = () => func(accInternal, head, counter, arrCopy);
+    return arrInternal.length === 0
+      ? accInternal
+      : reduceInternal(accNext(), tail, counter + 1);
+  })(acc, arrCopy, 0);
+};
+
+export default reduce;
